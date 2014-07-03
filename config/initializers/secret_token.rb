2dc70dba1981d5c84f76c5aee3cd67289062f942
heinlinaung <heinlinaung6891@gmail.com>
2014-07-03 19:49:34 +0630

    ygn_bookstore initial commit(generated)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YgnBookstore::Application.config.secret_key_base = '26739de3c89eef81ea4193524a2697ae0cd4442cc0ed463ab33219a86ea908ec438644ff2bb148c41e31d9fee39f2b626de71fca0e1c1d7608778358a61608e2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YgnBookstore::Application.config.session_store :cookie_store, key: '_ygn_bookstore_session'
