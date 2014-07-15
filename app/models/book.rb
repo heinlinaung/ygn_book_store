855d241e17c8b6e3c350384c3b1b6c74ffc92f00
heinlinaung <heinlinaung6891@gmail.com>
2014-07-15 11:29:28 +0630

    fixed for calling books with Descending by created date in Book Model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,5 @@
 class Book < ActiveRecord::Base
-	default_scope :order => 'title' #Order on Load
+	default_scope :order => 'created_at DESC' #Order on Load
 	validates :title, :description, :image_url, :presence => true
 	validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 	validates :image_url, :format => {:with=> %r{\.(gif|jpg|png)}i,:message => 'must upload supported file (GIF, JPG or PNG image).'}
