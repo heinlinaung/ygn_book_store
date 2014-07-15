474f77d6ccfa01b1d781be48f8eaf6870c583bcb
heinlinaung <heinlinaung6891@gmail.com>
2014-07-15 09:20:26 +0630

    generated Books Model, apply bootstrap and seed data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 YgnBookstore::Application.routes.draw do
+  resources :books
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
