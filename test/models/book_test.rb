61c7331206e3e66f167c83ac3af101960584dcfe
heinlinaung <heinlinaung6891@gmail.com>
2014-07-15 11:13:34 +0630

    generated Store Controller and index page and define as root

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -4,4 +4,12 @@ class BookTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "book attributes must not be empty" do book = Book.new
+  assert book.invalid?
+  assert book.errors[:title].any?
+    assert book.errors[:description].any?
+    assert book.errors[:price].any?
+    assert book.errors[:image_url].any?
+  end
 end
