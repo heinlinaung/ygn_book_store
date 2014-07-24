08aed30d0e5648cdd8d4bacc00f0420317a282de
heinlinaung <heinlinaung6891@gmail.com>
2014-07-24 12:40:38 +0630

    db migrated for line_item

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140724055106) do
+ActiveRecord::Schema.define(version: 20140724060938) do
 
   create_table "books", force: true do |t|
     t.string   "title"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20140724055106) do
   end
 
   create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "book_id"
+    t.integer  "cart_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
